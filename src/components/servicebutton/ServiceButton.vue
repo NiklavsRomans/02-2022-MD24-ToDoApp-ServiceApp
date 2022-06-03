266efcebd6d50990946acf0882f941f528c54f19
Niklavs Romans <niklavsromans19@gmail.com>
2022-06-03 02:31:00 +0200

    added button component

---
+++ src/components/servicebutton/ServiceButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="service-button">
+    <span>{{ title }}</span>
+    <span>{{ `$${price.toFixed(2)}` }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ServiceButton",
+  props: {
+    title: String,
+    price: Number,
+    active: Boolean,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "ServiceButton.scss";
+</style>
