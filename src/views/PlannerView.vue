cb5c89378bfd2c1ded55a8479aac907ff379efd6
Niklavs Romans <niklavsromans19@gmail.com>
2022-06-03 02:32:10 +0200

    added both app views

---
+++ src/views/PlannerView.vue
@@ -0,0 +1,15 @@
+<template>
+  <PlannerApp />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import PlannerApp from "@/components/Planner.vue";
+
+export default defineComponent({
+  name: "PlannerView",
+  components: {
+    PlannerApp,
+  },
+});
+</script>
---
+++ src/views/ServiceView.vue
@@ -0,0 +1,13 @@
+<template>
+  <ServicePlanner />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ServicePlanner from "@/components/Services.vue";
+
+export default defineComponent({
+  name: "ServiceView",
+  components: { ServicePlanner },
+});
+</script>
